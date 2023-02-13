674fe1ea2f0559046dc5bc885bc5e7af1fa9dbe3
JLToledano <jl.toledano.2018@alumnos.urjc.es>
2023-02-13 22:25:03 +0100

    Expansion of minimum sample of examples by results in metrics

--- mentalapp/main.py
+++ mentalapp/main.py
@@ -35,7 +35,7 @@ def dataset_initialize():
     #All data are read from the source file
     raw_data = complete_dataset.read_file(configuration_main['FILE_DATASET_NAME'])
     #A sample of the data is taken for testing.
-    raw_data = raw_data[0:10]
+    raw_data = raw_data[0:20]
     #The data is divided between training and evaluation. The parameter test_size marks the percent per one of data for evaluation.
     train_raw_data,test_raw_data = train_test_split(raw_data, test_size = 0.2, random_state = configuration_main['RANDOM_SEED'])
     
